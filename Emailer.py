0244a0007eb036be4ba02362c25626a6e4a5867e
matthew-rosenberg <48079079+matthew-rosenberg@users.noreply.github.com>
2020-12-19 23:06:33 -0500

    Updated the regex email checker

--- Emailer.py
+++ Emailer.py
@@ -15,7 +15,8 @@ from time import sleep
 # https://www.geeksforgeeks.org/check-if-email-address-valid-or-not-in-python/
 def check_email(email):  
     # Make a regular expression for validating an Email 
-    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    # https://stackoverflow.com/questions/201323/how-to-validate-an-email-address-using-a-regular-expression/201378#201378
+    regex = """(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
     if(re.search(regex,email)):  
         return True          
     else:  
@@ -89,7 +90,7 @@ def create_message(sender, to, subject, message_text, name=None, verbose=True):
   b64_bytes = base64.urlsafe_b64encode(message.as_bytes())
   b64_string = b64_bytes.decode()
   return {'raw': b64_string} 
-    #https://stackoverflow.com/questions/46668084/how-do-i-properly-base64-encode-a-mimetext-for-gmail-api, why decode and encode
+  #https://stackoverflow.com/questions/46668084/how-do-i-properly-base64-encode-a-mimetext-for-gmail-api, why decode and encode
 
 def send_message(service, user_id, message, verbose=True):
   """Send an email message.
@@ -103,12 +104,10 @@ def send_message(service, user_id, message, verbose=True):
   Returns:
     Sent Message.
   """
-  try:
-    message = (service.users().messages().send(userId=user_id, body=message)
-               .execute())
-    if verbose:
-      print (f"Message Id: {message['id']}")
-    return message
+  message = (service.users().messages().send(userId=user_id, body=message).execute())
+  if verbose:
+    print (f"Message Id: {message['id']}")
+  return message
 
 def main():
     #constants
